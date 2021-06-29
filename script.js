63ac274ad0480404894c1e2cd0a776df1cdc76e2
nitinspacex <63893398+nitinspacex@users.noreply.github.com>
2021-06-29 21:32:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ function simple_interest(){
     var r = document.getElementById ("third").value;
     var si = parseInt((p*t*r)/100 );
     document.getElementById ('num').innerHTML ="Simple interest : "+si;
+    if (p <= 0) {
+        alert("Enter a Positive Nsumber only")
+    }
     
     }
